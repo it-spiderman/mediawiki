3998a5305d40a893458bcbf7d90b2c5bbbb4e40a
Leonid Verhovskij <l.verhovskij@gmail.com>
2017-09-28 16:29:28 +0200

    create missing array for bsfoundation core

--- settings.d/020-BlueSpiceFree.php
+++ settings.d/020-BlueSpiceFree.php
@@ -1,5 +1,7 @@
 <?php
-
+if(!isset($GLOBALS['wgParamDefinitions'])){
+  $GLOBALS['wgParamDefinitions'] = [];
+}
 require_once "$IP/extensions/BlueSpiceFoundation/BlueSpiceFoundation.php";
 require_once "$IP/extensions/BlueSpiceExtensions/BlueSpiceExtensions.php";
 //require_once "$IP/extensions/BlueSpiceTagCloud/BlueSpiceTagCloud.php"; //activate after repo have been changed to public one and files have been moved
