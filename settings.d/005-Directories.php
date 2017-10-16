eee3cb2ee1e20202c8be42d555d755a6ecd8c290
Leonid Verhovskij <l.verhovskij@gmail.com>
2017-10-16 12:57:25 +0200

    use dir settings for docker env

--- settings.d/005-Directories.php
+++ settings.d/005-Directories.php
@@ -1,9 +1,20 @@
 <?php
-
+//File: 005-Directories.local.php
 // Cache directory.
 // If this is not set database will be used.
 // Must be writable by webserver.
-$wgCacheDirectory = __DIR__ . "/../cache";
+
+$wgCacheDirectory = getenv( 'BLUESPICE_DATA_PATH' ) . "/cache";
+
+$wgUploadDirectory = getenv( 'BLUESPICE_DATA_PATH' ) . "/images";
+$wgUploadPath = "$wgScriptPath/nsfr_img_auth.php";
+$wgEnableUploads = true;
+
+define( 'BS_DATA_DIR', getenv( 'BLUESPICE_DATA_PATH' ) . "/data" );
+
+define( 'BSCONFIGDIR', getenv( 'BLUESPICE_DATA_PATH' ) . "/config" );
+define( 'BSDATADIR', BS_DATA_DIR );
+//..: compiled_templates (Widgets)
 
 // This often causes problems. Only if really needed.
 // Must be writable by webserver.
